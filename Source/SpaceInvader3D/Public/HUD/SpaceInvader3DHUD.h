c66fcc6d7c6a3c23b178e1614270442d5592a72f
Andy Strube <andystrube64@gmail.com>
2025-03-04 18:30:28 -0800

    Added ASpaceInvader3DHUD class

---
+++ Source/SpaceInvader3D/Public/HUD/SpaceInvader3DHUD.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "SpaceInvader3DHUD.generated.h"
+
+UCLASS()
+class SPACEINVADER3D_API ASpaceInvader3DHUD : public AHUD
+{
+	GENERATED_BODY()
+
+private:
+
+	// <AActor>
+	virtual void BeginPlay() override;
+	// </AActor>
+
+	UPROPERTY(EditAnywhere, Category = SpaceInvader3D)
+	TSubclassOf<class USpaceInvader3DOverlay> SpaceInvader3DOverlayClass;
+
+	UPROPERTY()
+	TObjectPtr<class USpaceInvader3DOverlay> SpaceInvader3DOverlay;
+};
