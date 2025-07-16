212574cfb1ab5f70687e60deba25c90b854473af
Andy Strube <andystrube64@gmail.com>
2025-07-15 23:03:34 -0700

    Refactor

--- Source/SpaceInvader3D/Private/GameStates/SpaceInvaderGameState.cpp
+++ Source/SpaceInvader3D/Private/GameStates/SpaceInvaderGameState.cpp
@@ -1,2 +1,55 @@
 #include "GameStates/SpaceInvaderGameState.h"
+#include "Components/SphereComponent.h"
+#include "PatrolTargets/PatrolTarget.h"
+#include "Development/Development.h"
 
+ASpaceInvaderGameState::ASpaceInvaderGameState() {
+	PrimaryActorTick.bCanEverTick = true;
+
+	DetectionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("Detection Sphere"));
+	DetectionSphere->SetupAttachment(GetRootComponent());
+}
+
+void ASpaceInvaderGameState::BeginPlay() {
+	Super::BeginPlay();
+	SetupDetectionSphereOverlapFunctionality();
+}
+
+void ASpaceInvaderGameState::Tick(float DeltaTime) {
+	Super::Tick(DeltaTime);
+	HandleDetectionSphere();
+}
+
+void ASpaceInvaderGameState::SetupDetectionSphereOverlapFunctionality() {
+	if (DetectionSphere) {
+		DetectionSphere->OnComponentBeginOverlap.AddDynamic(this, &ASpaceInvaderGameState::HandleDetectionSphereOverlaps);
+	}
+}
+
+void ASpaceInvaderGameState::HandleDetectionSphereOverlaps(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) {
+	if (OtherActor) {
+		if (const TObjectPtr<APatrolTarget> PatrolTarget = Cast<APatrolTarget>(OtherActor)) {
+			PatrolTargets.Add(PatrolTarget);
+		}
+	}
+}
+
+void ASpaceInvaderGameState::HandleDetectionSphere() {
+	if (DetectionSphere) {
+		PerformDetectionSpherePing();
+		HandleDiscardingDetectionSphere();
+	}
+}
+
+void ASpaceInvaderGameState::PerformDetectionSpherePing() {
+	DetectionSphere->SetSphereRadius(
+		DetectionSphere->GetUnscaledSphereRadius() + 100000.f
+	);
+}
+
+void ASpaceInvaderGameState::HandleDiscardingDetectionSphere() {
+	if (DetectionSphere->GetUnscaledSphereRadius() >= 500000.f) {
+		DetectionSphere->DestroyComponent();
+		DetectionSphere = nullptr;
+	}
+}
--- Source/SpaceInvader3D/Public/GameStates/SpaceInvaderGameState.h
+++ Source/SpaceInvader3D/Public/GameStates/SpaceInvaderGameState.h
@@ -8,8 +8,41 @@ UCLASS()
 class SPACEINVADER3D_API ASpaceInvaderGameState : public AGameStateBase
 {
 	GENERATED_BODY()
+
+public:
+	ASpaceInvaderGameState();
+
+	//<AActor>
+	virtual void Tick(float DeltaTime) override;
+	//</AActor>
 	
 private:
+	// <AActor>
+	virtual void BeginPlay() override;
+	// </AActor>
+
+	// Delegate callbacks
+
+	UFUNCTION()
+	void HandleDetectionSphereOverlaps(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	// Functions
+
+	void SetupDetectionSphereOverlapFunctionality();
+	void HandleDetectionSphere();
+	void PerformDetectionSpherePing();
+	void HandleDiscardingDetectionSphere();
+
+	// Components
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<class USphereComponent> DetectionSphere;
+
+	// Other variables
+
+	UPROPERTY(EditAnywhere, Category = "Patrol Targets")
+	TArray<TObjectPtr<class APatrolTarget>> PatrolTargets;
+
 	UPROPERTY()
 	int32 EnemyShipCount;
 
